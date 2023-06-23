503ddce1ac7e832e902c25eebb803f6fbd833704
Gaga <23i03g@mail.ru>
2023-06-23 23:53:07 +0300

    save project

--- app/my-app/server/server.js
+++ app/my-app/server/server.js
@@ -105,7 +105,7 @@ const updateProject = async (req, res) => {
   var p = req.body.project;
   console.log({objectId}, p)
 
-  var result = await ProjectModel.updateById(objectId, p)
+  var result = await ProjectModel.findOneAndUpdate(objectId, p)
 
   res.json({
     result
