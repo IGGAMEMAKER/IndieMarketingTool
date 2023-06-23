021f68a480a73347849dec6309b51a10484c497d
Gaga <23i03g@mail.ru>
2023-06-23 23:47:38 +0300

    Site update:

--- app/my-app/src/PingBrowser.js
+++ app/my-app/src/PingBrowser.js
@@ -25,21 +25,36 @@ const ping = (url, picker) => {
     })
 }
 
-const update = (url, picker) => {
-  var t0 = new Date();
+// const update = (url, picker) => {
+//   var t0 = new Date();
+//
+//   return request
+//     .put((FRONTEND + url))
+//     .set('Access-Control-Allow-Origin', '*')
+//     .then(response => {
+//       var b = picker ? picker(response) : response.body;
+//
+//       var t1 = new Date();
+//       var diff_ms = t1.getTime() - t0.getTime();
+//
+//       // console.log('Data update took ' + diff_ms + 'ms');
+//
+//       return b;
+//     })
+//     .catch(err => {
+//       //console.error('ERROR IN PING.BROWSER.JS', Object.keys(err), err.status, err);
+//
+//       return [];
+//     })
+// }
 
+const update = (url, parameters = {}) => {
   return request
-    .put((FRONTEND + url))
+    .put(FRONTEND + url)
     .set('Access-Control-Allow-Origin', '*')
+    .send(parameters)
     .then(response => {
-      var b = picker ? picker(response) : response.body;
-
-      var t1 = new Date();
-      var diff_ms = t1.getTime() - t0.getTime();
-
-      // console.log('Data update took ' + diff_ms + 'ms');
-
-      return b;
+      return response.body;
     })
     .catch(err => {
       //console.error('ERROR IN PING.BROWSER.JS', Object.keys(err), err.status, err);
