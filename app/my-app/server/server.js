47e1a6625dde19a397729d8ba2253463c8b44574
Gaga <23i03g@mail.ru>
2023-06-25 21:41:01 +0300

    removing projects

--- app/my-app/server/server.js
+++ app/my-app/server/server.js
@@ -116,7 +116,7 @@ const removeProject = async (req, res) => {
   var projectId = req.params.objectId;
   console.log('WILL TRY TO REMOVE', {projectId})
 
-  ProjectModel.deleteById(projectId).then(r => {
+  ProjectModel.deleteOne({_id: projectId}).then(r => {
     console.log('REMOVED', {r})
     res.json({ok: 1})
   })
