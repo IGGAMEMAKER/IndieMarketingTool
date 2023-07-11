e8df2dc6921ca8c167a99539a8797d49e4cef00c
Gaga <23i03g@mail.ru>
2023-07-11 14:43:54 +0300

    Site update:

--- app/my-app/src/FieldPicker.js
+++ app/my-app/src/FieldPicker.js
@@ -46,7 +46,7 @@ export function NumberPicker({value, onAction, placeholder, normalValueRenderer,
 
   if (!editName) {
     if (normalValueRenderer)
-      return normalValueRenderer(onChangeName)
+      return <span className="onHover">{normalValueRenderer(onChangeName)}</span>
 
     return <label className="onHover" onClick={() => onChangeName(true)}>{value}</label>
   }
