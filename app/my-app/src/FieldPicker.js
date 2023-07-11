f26bef83470bca77063a9d2d655cecde8a97309c
Gaga <23i03g@mail.ru>
2023-07-11 18:23:08 +0300

    Site update:

--- app/my-app/src/FieldPicker.js
+++ app/my-app/src/FieldPicker.js
@@ -8,7 +8,7 @@ export function FieldPicker({value, onAction, onRemove, placeholder, autoFocus=t
     if (!normalValueRenderer)
       return <span className="editable" onClick={onChangeName}>{value}</span>
 
-    return <span className="editable">{normalValueRenderer(onChangeName)}</span>
+    return <div className="editable">{normalValueRenderer(onChangeName)}</div>
   }
 
   var saveButton = ''
