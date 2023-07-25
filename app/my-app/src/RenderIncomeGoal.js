cbe042ede147319e052fad7be198936c8c4d57b9
Gaga <23i03g@mail.ru>
2023-07-25 17:39:44 +0300

    Site update:

--- app/my-app/src/RenderIncomeGoal.js
+++ app/my-app/src/RenderIncomeGoal.js
@@ -1,3 +1,30 @@
+// export function renderIncomeGoal(project, goal, goalName) {
+//   if (!goalName)
+//     goalName = <span>make <b>{goal}</b> monthly</span>
+//
+//   return <div>
+//     To {goalName}, you need one of
+//     <br/>
+//     <br/>
+//     <center>
+//       <table>
+//         <tbody>
+//           <tr>
+//             {project.monetizationPlans.filter(plan => plan.price).map(plan => {
+//               var rounded = Math.ceil(goal / plan.price)
+//               return <td key={"plan-in-incomeee." + plan.id}>
+//                 <b>{plan.name}'s</b>
+//                 <br/>
+//                 {rounded}
+//               </td>
+//             })}
+//           </tr>
+//         </tbody>
+//       </table>
+//     </center>
+//   </div>
+// }
+
 export function renderIncomeGoal(project, goal, goalName) {
   if (!goalName)
     goalName = <span>make <b>{goal}</b> monthly</span>
@@ -7,20 +34,13 @@ export function renderIncomeGoal(project, goal, goalName) {
     <br/>
     <br/>
     <center>
-      <table>
-        <tbody>
-          <tr>
-            {project.monetizationPlans.filter(plan => plan.price).map(plan => {
-              var rounded = Math.ceil(goal / plan.price)
-              return <td key={"plan-in-incomasdasdasd." + plan.id}>
-                <b>{plan.name}'s</b>
-                <br/>
-                {rounded}
-              </td>
-            })}
-          </tr>
-        </tbody>
-      </table>
+      {project.monetizationPlans.filter(plan => plan.price).map(plan => {
+        var rounded = Math.ceil(goal / plan.price)
+
+        return <div key={"plan-in-incomeee." + plan.id}>
+          <b>{plan.name}'s</b>: {rounded}
+        </div>
+      })}
     </center>
   </div>
 }
