3cc8061701587632819fc058b1fcb9a6d5a98647
Gaga <23i03g@mail.ru>
2023-08-08 13:53:59 +0300

    Site update:

--- app/my-app/src/ProjectList.js
+++ app/my-app/src/ProjectList.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 
 export function ProjectList({projectIDs}) {
-  var list = projectIDs.map(({id, name}) => <li className={"project-link"}><Link to={"/projects/" + id}>{name}</Link></li>)
+  var list = projectIDs.filter(p => p.name).map(({id, name}) => <li className={"project-link"}><Link to={"/projects/" + id}>{name}</Link></li>)
 
   return <ul>{list}</ul>
 }
