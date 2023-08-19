443d815e89f6b7301313e763d28e647e2a573864
Gaga <23i03g@mail.ru>
2023-08-19 10:11:42 +0300

    Site update:

--- app/my-app/src/RiskView.js
+++ app/my-app/src/RiskView.js
@@ -24,64 +24,75 @@ function RiskSolutionAdder({riskIndex}) {
 }
 
 export function RiskView({risk, index, it, goal, orderingAllowed = true}) {
-  var id = risk.id;
-  var solutions = risk.solutions || []
+  try {
+    var id = risk.id;
+    var solutions = risk.solutions || []
 
-  var renderer = onChange => {
-    var solutionRenderer;
+    var renderer = onChange => {
+      var solutionRenderer;
 
-    if (solutions.length) {
-      solutionRenderer = solutions.map(s => <li key={"risk." + id + ".solution" + s.id} className="text-secondary">
-        <FieldPicker
-          value={s.name}
-          placeholder={"Add possible solution"}
-          onAction={val => actions.editRiskSolution(id, s.id, val)}
-          onRemove={() => actions.removeRiskSolution(id, s.id)}
-        />
-      </li>)
-    }
+      if (solutions.length) {
+        solutionRenderer = solutions.map(s => <li key={"risk." + id + ".solution" + s.id} className="text-secondary">
+          <FieldPicker
+            value={s.name}
+            placeholder={"Add possible solution"}
+            onAction={val => actions.editRiskSolution(id, s.id, val)}
+            onRemove={() => actions.removeRiskSolution(id, s.id)}
+          />
+        </li>)
+      }
 
-    var movementBar;
-    if (orderingAllowed) {
-      var up = <button onClick={() => actions.changeRiskOrder(index, index - 1)}>Up</button>
-      var down = <button onClick={() => actions.changeRiskOrder(index, index + 1)}>Down</button>
+      var movementBar;
+      if (orderingAllowed) {
+        var up = <button onClick={() => actions.changeRiskOrder(index, index - 1)}>Up</button>
+        var down = <button onClick={() => actions.changeRiskOrder(index, index + 1)}>Down</button>
 
-      movementBar = <span>{up} {down}</span>
-    }
+        movementBar = <span>{up} {down}</span>
+      }
 
-    const removeButton = <button style={{float: 'right'}} onClick={() => {actions.removeRisk(id)}}>x</button>
-    var removeFromIterationButton;
-    if (it) {
-      removeFromIterationButton = <button style={{float: 'right'}} onClick={() => {actions.removeIterationGoal(it.id, goal.id)}}>remove from iteration</button>
-    }
-
-    return <div>
-      <div>
-        <span onClick={() => onChange(true)}>{risk.name}</span>
-        {movementBar}{removeFromIterationButton}
-      </div>
-      <ul>
-        {solutionRenderer}
-        <RiskSolutionAdder riskIndex={id}/>
-      </ul>
-    </div>
-  }
-
-  var content = <FieldPicker
-    value={risk?.name}
-    normalValueRenderer={renderer}
-    placeholder={"Risk"}
-    onAction={val => actions.editRiskName(id, val)}
-    onRemove={() => {
-      if (!it)
+      const removeButton = <button style={{float: 'right'}} onClick={() => {
         actions.removeRisk(id)
-    }}
-    key={'risk' + id}
-  />
+      }}>x</button>
+      var removeFromIterationButton;
+      if (it) {
+        removeFromIterationButton = <button style={{float: 'right'}} onClick={() => {
+          actions.removeIterationGoal(it.id, goal.id)
+        }}>remove from iteration</button>
+      }
 
-  return (
-    <li className="Risk-item">
-      {content}
-    </li>
-  )
+      return <div>
+        <div>
+          <span onClick={() => onChange(true)}>{risk.name}</span>
+          {movementBar}{removeFromIterationButton}
+        </div>
+        <ul>
+          {solutionRenderer}
+          <RiskSolutionAdder riskIndex={id}/>
+        </ul>
+      </div>
+    }
+
+    var content = <FieldPicker
+      value={risk?.name}
+      normalValueRenderer={renderer}
+      placeholder={"Risk"}
+      onAction={val => actions.editRiskName(id, val)}
+      onRemove={() => {
+        if (!it)
+          actions.removeRisk(id)
+      }}
+      key={'risk' + id}
+    />
+
+    return (
+      <li className="Risk-item">
+        {content}
+      </li>
+    )
+  }
+  catch (e) {
+    console.log('NO RISK', risk, index, it, goal, orderingAllowed)
+
+    return <div>error in risk</div>
+  }
 }
