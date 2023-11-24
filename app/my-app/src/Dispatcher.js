7030c8c7061c1f66fc6826789ce4800b046b3bfb
Gaga <23i03g@mail.ru>
2023-11-24 22:05:36 +0300

    restore flux

--- app/my-app/src/Dispatcher.js
+++ app/my-app/src/Dispatcher.js
@@ -1,16 +1,17 @@
-// import { Dispatcher } from 'flux';
+import { Dispatcher } from 'flux';
 
 // "cors": "^2.8.5",
 //   "express": "^4.18.2",
 //   "flux": "^4.0.4",
-class Dispatcher {
-  dispatch() {
 
-  }
-
-  register() {
-
-  }
-}
+// class Dispatcher {
+//   dispatch() {
+//
+//   }
+//
+//   register() {
+//
+//   }
+// }
 
 export default new Dispatcher();
