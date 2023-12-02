cd8c675643aa364fb3d09062e80abda97baddb78
Gaga <23i03g@mail.ru>
2023-12-02 20:15:03 +0300

    Site update:

--- app/my-app/src/AdminPage.js
+++ app/my-app/src/AdminPage.js
@@ -25,6 +25,13 @@ export class AdminPage extends Component {
     var users = this.state.grouped;
 
     var projectsCount = users.map(u => u.count).reduce((p, c) => p + c, 0)
+    const getEmail = u => {
+      try {
+        return u?.user?.email.split('@')[0]
+      } catch (e) {
+        return u?.user?.email
+      }
+    }
 
     return <div>
       <h1>Admin</h1>
@@ -34,7 +41,7 @@ export class AdminPage extends Component {
         {users.sort((u1, u2) => u2.count - u1.count).map(u => <tr>
           <td style={{textAlign: 'right'}}>
             {/*<b>{u._id} {JSON.stringify(u?.user, null, 2)} /!*{JSON.stringify(u, null, 2)}*!/</b>*/}
-            <b>{u?.user?.email.split('@')[0]}{/*{JSON.stringify(u, null, 2)}*/}</b>
+            <b>{getEmail(u)}{/*{JSON.stringify(u, null, 2)}*/}</b>
           </td>
           <td>
             [{u.count}]
