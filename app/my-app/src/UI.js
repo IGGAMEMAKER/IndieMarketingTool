c9c13d907040728ca5af5db3fb459b07e9cd3de4
Gaga <23i03g@mail.ru>
2023-12-02 17:17:12 +0300

    Site update: auth faster for me

--- app/my-app/src/UI.js
+++ app/my-app/src/UI.js
@@ -1,4 +1,6 @@
 import {Link} from "react-router-dom";
+import {Component} from "react";
+import {render} from "react-dom";
 
 export const col1 = t => <span className="color1">{t}</span>
 export const col2 = t => <span className="color2">{t}</span>
@@ -28,24 +30,35 @@ export function ButtonLink ({url, text}) {
   return b
 }
 
-export function TryItButton() {
-  return <div>
-    <table>
-      <tr>
-        {/*<td>{loginForm}</td>*/}
-        <td>
-          <ButtonLink url={"/login"} text={"Try it!"} />
-        </td>
-        {/*<td></td>*/}
-        {/*<td></td>*/}
-        {/*<td>*/}
-        {/*  <Link target="_blank" to={"/about"}>*/}
-        {/*    {col2("More info")}*/}
-        {/*  </Link>*/}
-        {/*</td>*/}
-      </tr>
-    </table>
-  </div>
+export class TryItButton extends Component {
+  // componentDidMount() {
+  //
+  // }
+
+  render() {
+    const hasCookies = document.cookie("userId") || document.cookie("email")
+    let authButton = <ButtonLink url={"/login"} text={"Try it!"} />
+    if (hasCookies)
+      authButton = <ButtonLink url={"/profile"} text={"Profile"} />
+
+    return <div>
+      <table>
+        <tr>
+          {/*<td>{loginForm}</td>*/}
+          <td>
+            {authButton}
+          </td>
+          {/*<td></td>*/}
+          {/*<td></td>*/}
+          {/*<td>*/}
+          {/*  <Link target="_blank" to={"/about"}>*/}
+          {/*    {col2("More info")}*/}
+          {/*  </Link>*/}
+          {/*</td>*/}
+        </tr>
+      </table>
+    </div>
+  }
 }
 
 // module.exports = {
