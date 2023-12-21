a57ac2ab59e0ae67910c4e68401cf04da085db2e
Gaga <23i03g@mail.ru>
2023-12-21 22:03:07 +0300

    Site update: split growth panel by modes

--- app/my-app/src/AudienceChannelsList.js
+++ app/my-app/src/AudienceChannelsList.js
@@ -1,8 +1,10 @@
 import {ChannelList} from "./ChannelList";
+import {Panel} from "./Panel";
 
 export function AudienceChannelsList({channels}) {
   return <div>
-    <h3>Where will you find your audience?</h3>
+    <Panel id="GROWTH" header="Where will you find your audience?"/>
+    {/*<h3>Where will you find your audience?</h3>*/}
     <div className="Container">
       <ChannelList channels={channels}/>
     </div>
